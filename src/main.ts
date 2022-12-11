44da0677ffe9e1e46a0d8b4ce61910294b2ba44d
HKLab <60681033+HKLab@users.noreply.github.com>
2022-12-11 12:55:27 +0800

    v0

---
+++ src/main.ts
@@ -0,0 +1,175 @@
+
+import extra from 'fs-extra'
+import axios, { AxiosRequestHeaders } from 'axios';
+import { ModLinksData, ModLinksManifestData, parseModLinks } from './modlinks.js';
+import { existsSync } from 'fs';
+
+function getJsonPath() {
+    return 'modlinks.json';
+}
+
+interface CommitInfo {
+    sha: string;
+    commit: {
+        author: {
+            date: string;
+            name: string;
+            email: string;
+        };
+        tree: {
+            url: string;
+        };
+    }
+}
+
+interface TreeInfo {
+    tree: {
+        path: string;
+        url: string;
+    }[];
+}
+
+interface FileContent {
+    content: string
+}
+
+interface ModCollection {
+    mods: Record<string, ModVersionCollection>;
+    latestCommit?: string;
+}
+
+type ModVersionCollection = Record<string, ModLinksManifestData>;
+
+//process.exit();
+const token = process.env.GH_TOKEN;
+if (token) {
+    axios.defaults.headers.common['Authorization'] = `token ${token}`;
+} else {
+    console.error('Require GH_TOKEN');
+    process.abort();
+}
+
+
+let allCommits: CommitInfo[] = [];
+type ModLinksResult = { data: ModLinksData, commit: CommitInfo } | string;
+
+let modrecord: ModCollection = {
+    mods: {}
+};
+
+if (existsSync(getJsonPath())) {
+    modrecord = extra.readJSONSync(getJsonPath());
+    console.log('Has cache');
+}
+
+
+let page = 0;
+let breakGet = false;
+let rate = 5000;
+let resetTime = 0;
+
+let endCommit = modrecord.latestCommit ?? '1a7399ead20a40c363091a5f0afea8be923f5ea8';
+
+console.log(`Fetch commits`);
+while (!breakGet && (allCommits.length < 100 || process.env.MODLINK_FULL)) {
+    const result = await axios.get<CommitInfo[]>(`https://api.github.com/repos/hk-modding/modlinks/commits?page=${page}&per_page=100`);
+    const header = result.headers as AxiosRequestHeaders;
+    rate = Number.parseInt(header.get('x-ratelimit-remaining', undefined)?.toString() ?? '0')
+    resetTime = Number.parseInt(header.get('x-ratelimit-reset', undefined)?.toString() ?? '0')
+    const req = result.data;
+    if (req.length == 0) break;
+    page++;
+    for (const commit of req) {
+        if (commit.sha === endCommit) {
+            breakGet = true;
+            break;
+        }
+        allCommits.push(commit);
+    }
+}
+console.log(`x-ratelimit-reset: ${new Date(resetTime * 1000).toLocaleString()} ${resetTime}`);
+console.log(`x-ratelimit-remaining: ${rate}`);
+console.log(`Total commits: ${allCommits.length} ${page}`);
+
+if (allCommits.length == 0) {
+    console.log('Nothing can do');
+    process.exit(0);
+}
+
+console.log(`Fetch ModLinks`);
+
+const modlinks: (ModLinksResult | Promise<ModLinksResult>)[] = [];
+modlinks.length = allCommits.length;
+
+async function fetchCommit(cid: number, commit: CommitInfo) {
+    const r = modlinks[cid];
+    if (r) {
+        if (r instanceof Promise) return await r;
+        return r;
+    }
+    return await (modlinks[cid] = (async function (id) {
+        try {
+            const tree = (await axios.get<TreeInfo>(commit.commit.tree.url)).data;
+            const ml = tree.tree.find(x => x.path.toLowerCase() == 'modlinks.xml');
+            if (!ml) throw `Not found modlinks.xml in ${commit.sha}`;
+            const content = Buffer.from((await axios.get<FileContent>(ml.url)).data.content, 'base64').toString('utf-8');
+            console.log(`[${id}] ${commit.sha} is done`);
+            const result = modlinks[id] = {
+                data: await parseModLinks(content),
+                commit: commit
+            };
+            //Find Next
+            let n_id = modlinks.findIndex(x => x == undefined);
+            if (n_id > 0 && n_id < allCommits.length) {
+                fetchCommit(n_id, allCommits[n_id]);
+            }
+            return result;
+        } catch (e) {
+            console.log(e);
+            modlinks[id] = e.toString();
+            return modlinks[id];
+        }
+    })(cid++));
+}
+
+for (let i = 0; i < 20; i++) {
+    fetchCommit(i, allCommits[i]);
+}
+
+let missing = 0;
+for (let i = 0; i < allCommits.length; i++) {
+    let result: ModLinksResult = await fetchCommit(i, allCommits[i]);
+    if (typeof result == 'string') {
+        missing++;
+        continue;
+    }
+    const { data, commit } = result;
+    const mods = data.mods;
+    for (const mod of mods) {
+        let mvs = modrecord.mods[mod.name];
+        if (!mvs) {
+            modrecord.mods[mod.name] = mvs = {};
+        }
+        if (!mvs[mod.version]) {
+            mvs[mod.version] = mod;
+            if (i > 0) {
+                console.log(`[Mod]${mod.name} - ${mod.version}`);
+            } else {
+                modrecord.latestCommit = commit.sha;
+            }
+            mod.date = commit.commit.author.date;
+        }
+    }
+    if (i % 50 == 0) {
+        extra.outputJSONSync(getJsonPath(), modrecord, {
+            spaces: 4
+        });
+    }
+    console.log(`(${i++})${commit.commit.author.date} '${commit.commit.author.name}' ${commit.sha}`);
+}
+
+extra.outputJSONSync(getJsonPath(), modrecord, {
+    spaces: 4
+});
+
+console.log(`${missing} is missing`);
---
+++ src/modlinks.ts
@@ -0,0 +1,113 @@
+
+import process from 'process';
+import { Parser, ast } from 'tsxml'
+
+type ContainerNode = ast.ContainerNode<ast.Node>;
+type TextNode = ast.TextNode;
+type CDataNode = ast.CDataSectionNode;
+
+export type ModTag = "Boss" | "Cosmetic" | "Expansion" | "Gameplay" | "Library" | "Utility";
+
+export let currentPlatform: string = "Windows";
+
+export interface ModLinksManifestData {
+    name: string;
+    desc: string;
+    version: string;
+    link: string;
+    dependencies: string[];
+    repository: string | undefined;
+    integrations: string[];
+    tags: ModTag[];
+    authors: string[];
+    date?: string;
+}
+
+export class ModLinksData {
+    mods: ModLinksManifestData[] = [];
+
+}
+
+
+function findXmlNode<T = Node>(parent: ContainerNode, tagName: string): T | undefined {
+    return (parent.childNodes.find(x => x.tagName === tagName) as (T | undefined));
+}
+
+function getXmlNodeText(parent: ContainerNode, tagName: string): string | undefined {
+    return (findXmlNode<ContainerNode>(parent, tagName)?.childNodes[0] as (TextNode | undefined))?.content;
+}
+
+function getCDATANodeText(parent: ContainerNode, tagName: string): string | undefined {
+    return (findXmlNode<ContainerNode>(parent, tagName)?.childNodes[0] as (CDataNode | undefined))?.content;
+}
+
+export async function parseModLinks(content: string): Promise<ModLinksData> {
+    const result = new ModLinksData();
+    const xml = await Parser.parseString(content);
+    const root = xml.getAst().childNodes[1] as ContainerNode;
+    if (!root) throw 0;
+    for (let i = 0; i < root.childNodes.length; i++) {
+        const node = root.childNodes[i];
+        if (node instanceof ast.CommentNode) continue;
+        const manifest = node as ContainerNode;
+        const mod: ModLinksManifestData = {
+            name: "",
+            desc: "",
+            version: "",
+            link: "",
+            dependencies: [],
+            repository: "",
+            integrations: [],
+            tags: [],
+            authors: []
+        };
+        mod.name = getXmlNodeText(manifest, "Name") ?? "";
+        mod.desc = getXmlNodeText(manifest, "Description") ?? "";
+        mod.version = getXmlNodeText(manifest, "Version") ?? "";
+        let tlink = getCDATANodeText(manifest, "Link");
+        if (!tlink) {
+            const nlinks = findXmlNode<ContainerNode>(manifest, "Links");
+            tlink = nlinks ? getCDATANodeText(nlinks, currentPlatform) : undefined;
+            if (!tlink) continue;
+        }
+        mod.link = tlink;
+
+        const depNode = findXmlNode<ContainerNode>(manifest, "Dependencies");
+        if (depNode && !(depNode instanceof ast.SelfClosingNode)) {
+            for (let i2 = 0; i2 < depNode.childNodes.length; i2++) {
+                const dep = (depNode.childNodes[i2] as ContainerNode).childNodes[0] as TextNode;
+                mod.dependencies.push(dep.content);
+            }
+        }
+
+        mod.repository = getCDATANodeText(manifest, "Repository");
+
+        const integrationsNode = findXmlNode<ContainerNode>(manifest, "Integrations");
+        if (integrationsNode) {
+            for (let i2 = 0; i2 < integrationsNode.childNodes.length; i2++) {
+                const integration = (integrationsNode.childNodes[i2] as ContainerNode).childNodes[0] as TextNode;
+                mod.integrations.push(integration.content);
+            }
+        }
+
+        const tagsNode = findXmlNode<ContainerNode>(manifest, "Tags");
+        if (tagsNode) {
+            for (let i2 = 0; i2 < tagsNode.childNodes.length; i2++) {
+                const tag = (tagsNode.childNodes[i2] as ContainerNode).childNodes[0] as TextNode;
+                mod.tags.push(tag.content as ModTag);
+            }
+        }
+
+        const authorsNode = findXmlNode<ContainerNode>(manifest, "Authors");
+        if (authorsNode) {
+            for (let i2 = 0; i2 < authorsNode.childNodes.length; i2++) {
+                const author = (authorsNode.childNodes[i2] as ContainerNode).childNodes[0] as TextNode;
+                mod.authors.push(author.content as ModTag);
+            }
+        }
+
+        result.mods.push(mod);
+    }
+    result.mods.sort((a, b) => a.name.localeCompare(b.name));
+    return result;
+}
